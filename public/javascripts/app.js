9658a1a00ef997049a2de33b6dc7772778243881
Mark Clement <clement@cs.byu.edu>
2019-06-26 21:51:27 -0600

    Update app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,40 +1,41 @@
-angular.module('comment', [])
-.controller('MainCtrl', [
-  '$scope','$http',
-  function($scope,$http){
-    $scope.test = 'Hello world!';
-	$scope.comments = [
-      {title:'Comment 1', upvotes:5},
-      {title:'Comment 2', upvotes:6},
-      {title:'Comment 3', upvotes:1},
-      {title:'Comment 4', upvotes:4},
-      {title:'Comment 5', upvotes:3}
-    ];
-    $scope.create = function(comment) {
-      return $http.post('/comments', comment).success(function(data){
-        $scope.comments.push(data);
-      });
-    };
-	$scope.addComment = function() {
-	  $scope.create({title:$scope.formContent,upvotes:0});
-      $scope.formContent='';
-    };
-    $scope.upvote = function(comment) {
-      return $http.put('/comments/' + comment._id + '/upvote')
-        .success(function(data){
-          console.log("upvote worked");
-          comment.upvotes = data.upvotes;
-        });
-    };
-	$scope.incrementUpvotes = function(comment) {
-	  $scope.upvote(comment);
-    };
-    $scope.getAll = function() {
-      return $http.get('/comments').success(function(data){
-        angular.copy(data, $scope.comments);
-      });
-    };
-    $scope.getAll();
+/*global axios */
+/*global Vue */
+var app = new Vue({
+    el: '#app',
+    data: {
+        comments: [
+            { title: 'Comment 1', upvotes: 5 },
+            { title: 'Comment 2', upvotes: 6 },
+            { title: 'Comment 3', upvotes: 1 },
+            { title: 'Comment 4', upvotes: 4 },
+            { title: 'Comment 5', upvotes: 3 }
+        ],
+        newComment: "",
+    },
+    created: function() {},
+    computed: {
+        sortedComments() {
+            return this.comments.sort((a, b) => {
+                var rval = 0;
+                if (a.upvotes > b.upvotes) {
+                    rval = 1;
+                }
+                else if (a.upvotes < b.upvotes) {
+                    rval = -1;
+                }
+                return (rval);
+            })
+        }
 
-  }
-]);
+    },
+    methods: {
+        addComment() {
+            this.comments.push({ title: this.newComment, upvotes: 0 });
+            this.newComment = "";
+        },
+        incrementUpvotes(item){
+            item.upvotes = item.upvotes+1;
+        }
+    }
+});
+
